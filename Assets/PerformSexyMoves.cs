861aa4594e537938f985ce8db4920b718c746926
Frederico Zveiter <frederico.zveiter@gmail.com>
2015-09-29 11:22:07 -0700

    Submitting test Unity 5.2.0f3 project.

---
+++ Assets/PerformSexyMoves.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerformSexyMoves : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = 
+            new Vector3(
+                Mathf.Sin(4 * Time.time), 
+                Mathf.Cos(5 * Time.time), 
+                Mathf.Sin(6 * Time.time)
+                );
+    }
+}
